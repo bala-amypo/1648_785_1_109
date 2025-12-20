34a178bacd5c6d4598c50e0c243cdea33cbd3f17
bala-amypo <bala@amypo.in>
2025-12-20 14:13:52 +0000

    User commit

--- src/main/java/com/example/demo/entity/creditcard.java
+++ src/main/java/com/example/demo/entity/creditcard.java
@@ -1,11 +1,11 @@
 package com.example.demo.entity;
 public class creditcard{
-    private Long id;
-    private String userid;
-    private String fullname;
-    private String email;
-    private String password;
-    private String role;
-    private Boolean active;
-    private LocalDateTime createAt; 
+    // private Long id;
+    // private String userid;
+    // private String fullname;
+    // private String email;
+    // private String password;
+    // private String role;
+    // private Boolean active;
+    // private LocalDateTime createAt; 
 }
