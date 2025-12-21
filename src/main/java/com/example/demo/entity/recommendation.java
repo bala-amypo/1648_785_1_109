2869ccf24f172524600cb13b74e7b5bcae254703
bala-amypo <bala@amypo.in>
2025-12-21 18:01:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/recommendation.java
+++ src/main/java/com/example/demo/entity/recommendation.java
@@ -1,4 +1,27 @@
 package com.example.demo.entity;
-public class recommendation{
-    
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.*;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private Long purchaseIntentId;
+    private Long recommendedCardId;
+    private Double expectedRewardValue;
+    private String calculationDetailsJson;
+    private LocalDateTime recommendedAt;
+
+    @PrePersist
+    void created() {
+        recommendedAt = LocalDateTime.now();
+    }
 }
