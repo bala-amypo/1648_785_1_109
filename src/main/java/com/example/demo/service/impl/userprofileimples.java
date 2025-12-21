b0a1395bb3ff28b1340897b9dd490c1c9598a6b2
bala-amypo <bala@amypo.in>
2025-12-21 13:10:02 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/userprofileimples.java
+++ src/main/java/com/example/demo/service/impl/userprofileimples.java
@@ -1,35 +1,51 @@
-package com.example.demo.service.imples;
+package com.example.demo.service.impl;
+
 import com.example.demo.entity.userprofile;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.userprofilerepository;
-import com.example.demo.service.userprofileservice;
+import com.example.demo.repository.UserProfileRepository;
+import com.example.demo.service.UserProfileService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springramework.beans.factory.annotation.Autowired;
+
 import java.util.List;
 
 @Service
-public class userprofileimples implements userprofileservice{
-@Autowired userprofilerepository users;
+public class UserProfileServiceImpl implements UserProfileService {
+
+    private final UserProfileRepository users;
+    private final PasswordEncoder passwordEncoder;
+
+    // Constructor Injection (BEST PRACTICE)
+    public UserProfileServiceImpl(UserProfileRepository users,
+                                  PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public userprofile createUser(userprofile profile) {
         profile.setPassword(passwordEncoder.encode(profile.getPassword()));
         profile.setActive(true);
         return users.save(profile);
     }
-     @Override
+
+    @Override
     public userprofile getUserById(Long id) {
-        return users.findById(id).orElseThrow(null);
+        return users.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     public userprofile findByUserId(String userId) {
-        return users.findByUserId(userId).orElseThrow(null);
+        return users.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
+
     @Override
     public List<userprofile> getAllUsers() {
         return users.findAll();
     }
+
     @Override
     public userprofile updateUserStatus(Long id, boolean active) {
         userprofile user = getUserById(id);
