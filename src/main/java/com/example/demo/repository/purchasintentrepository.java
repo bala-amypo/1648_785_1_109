3c8d1558edecb4fa46d5938c90048b685ef6c656
bala-amypo <bala@amypo.in>
2025-12-21 17:43:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/purchasintentrepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.purchasintent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface purchasintentrepository extends JpaRepository<purchasintent, Long> {
-
-}
--- src/main/java/com/example/demo/service/purchasintentservice.java
+++ src/main/java/com/example/demo/service/purchasintentservice.java
@@ -1,7 +1,15 @@
 package com.example.demo.service;
-public class purchasintentservice{
-    purchasintent createIntent(purchasintent intent);
-    purchasintent getIntentsByUser()
-    purchasintent
-    purchasintent
+
+import com.example.demo.entity.purchaseintent;
+import java.util.List;
+
+public interface purchaseintentservice {
+
+    purchaseintent createIntent(purchaseintent intent);
+
+    List<purchaseintent> getIntentsByUser(Long userId);
+
+    purchaseintent getIntentById(Long id);
+
+    List<purchaseintent> getAllIntents();
 }
