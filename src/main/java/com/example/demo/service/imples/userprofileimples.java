a9e1780bf19b2e09c7ecf6b3c542d2b3dbef0229
bala-amypo <bala@amypo.in>
2025-12-21 12:40:01 +0000

    User commit

--- src/main/java/com/example/demo/service/imples/userprofileimples.java
+++ src/main/java/com/example/demo/service/imples/userprofileimples.java
@@ -1,49 +1,54 @@
-// package com.example.demo.service;
+package com.example.demo.service.impl;
 
-// import com.example.demo.entity.UserProfile;
-// import com.example.demo.exception.ResourceNotFoundException;
-// import com.example.demo.repository.UserProfileRepository;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
+import com.example.demo.entity.UserProfile;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.UserProfileRepository;
+import com.example.demo.service.UserProfileService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class userprofileimles {
+import java.util.List;
 
-//     private final UserProfileRepository repository;
-//     private final PasswordEncoder passwordEncoder;
+@Service
+public class UserProfileServiceImpl implements UserProfileService {
+    public UserProfileServiceImpl(UserProfileRepository repository,
+                                  PasswordEncoder passwordEncoder) {
+        this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-//     public UserProfileService(UserProfileRepository repository,PasswordEncoder passwordEncoder) {
-//         this.repository = repository;
-//         this.passwordEncoder = passwordEncoder;
-//     }
+    @Override
+    public UserProfile createUser(UserProfile profile) {
+        profile.setPassword(passwordEncoder.encode(profile.getPassword()));
+        profile.setActive(true);
+        return repository.save(profile);
+    }
 
-//     public userprofile createUser(userprofile profile) {
-//         profile.setPassword(passwordEncoder.encode(profile.getPassword()));
-//         profile.setActive(true);
-//         return repository.save(profile);
-//     }
+    @Override
+    public UserProfile getUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 
-//     public userprofile getUserById(Long id) {
-//         return repository.findById(id)
-//                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-//     }
+    @Override
+    public UserProfile findByUserId(String userId) {
+        return repository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 
-//     public userprofile findByUserId(String userId) {
-//         return repository.findByUserId(userId)
-//                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-//     }
+    @Override
+    public List<UserProfile> getAllUsers() {
+        return repository.findAll();
+    }
 
-//     public List<userprofile> getAllUsers() {
-//         return repository.findAll();
-//     }
+    @Override
+    public UserProfile updateUserStatus(Long id, boolean active) {
+        UserProfile user = getUserById(id);
+        user.setActive(active);
+        return repository.save(user);
+    }
+}
 
-//     public userprofile updateUserStatus(Long id, boolean active) {
-//         userprofile user = getUserById(id);
-//         user.setActive(active);
-//         return repository.save(user);
-//     }
-// }
 
 package com.example.demo.service.imples;
 
@@ -51,6 +56,21 @@ package com.example.demo.service.imples;
 @Service
 public class userprofileimples implements userprofileservice{
 @Autowired userprofilerepository user;
-@Override
-public userprofile 
+    @Override
+    public UserProfile createUser(UserProfile profile) {
+        profile.setPassword(passwordEncoder.encode(profile.getPassword()));
+        profile.setActive(true);
+        return repository.save(profile);
+    }
+     @Override
+    public UserProfile getUserById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    @Override
+    public UserProfile findByUserId(String userId) {
+        return repository.findByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 }
