2937372d95123b6028ef0197e634067c7e6b7262
bala-amypo <bala@amypo.in>
2025-12-21 11:30:56 +0000

    User commit

--- src/main/java/com/example/demo/repository/creditcardrepository.java
+++ src/main/java/com/example/demo/repository/creditcardrepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-public interface creditcard extends JpaRepository<CreditCardRecord, Long> {
+public interface creditcardrepository extends JpaRepository<creditcard, Long> {
     List<creditcard> findByUserId(Long userId);
 }
 
