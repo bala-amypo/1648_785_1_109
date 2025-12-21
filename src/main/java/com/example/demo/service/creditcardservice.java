a56b865af18f51943ace83506a2fdcf5ccfc7599
bala-amypo <bala@amypo.in>
2025-12-21 15:22:00 +0000

    User commit

--- src/main/java/com/example/demo/service/creditcardservice.java
+++ src/main/java/com/example/demo/service/creditcardservice.java
@@ -1,4 +1,12 @@
 package com.example.demo.service;
-public class creditcardservice{
-    
+import com.example.demo.entity.creditcard;
+import java.util.List;
+
+public interface creditcardservice {
+
+    creditcard createUser(userprofile profile);
+    creditcard getUserById(Long id);
+    creditcard findByUserId(String userId);
+    List<creditcard> getAllUsers();
+    creditcard updateUserStatus(Long id, boolean active);
 }
