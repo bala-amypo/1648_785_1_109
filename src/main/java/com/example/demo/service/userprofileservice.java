7390c2114e9f5f021889dc16ba03d696847fcd52
bala-amypo <bala@amypo.in>
2025-12-21 12:22:01 +0000

    User commit

--- src/main/java/com/example/demo/service/userprofileservice.java
+++ src/main/java/com/example/demo/service/userprofileservice.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 import com.example.demo.entity.userprofile;
 public class userprofileservice{
+userprofile createUser(userprofile,profile);
+userprofile getUse
 userprofile findByUserId(String userId);
 }
