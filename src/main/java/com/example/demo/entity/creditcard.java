2400da3a29a82dde0760b7b7e8d5494adda2dcac
bala-amypo <bala@amypo.in>
2025-12-21 15:58:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/creditcard.java
+++ src/main/java/com/example/demo/entity/creditcard.java
@@ -1,10 +1,7 @@
 package com.example.demo.entity;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 @Entity
 @Data
 @AllArgsConstructor
