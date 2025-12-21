aae4db98c371df43a3b9a1c3b58c29e4dd842137
bala-amypo <bala@amypo.in>
2025-12-21 18:07:13 +0000

    User commit

--- src/main/java/com/example/demo/service/recommendationservice.java
+++ src/main/java/com/example/demo/service/recommendationservice.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 public class recommendationservice{
-    recommendation
-    recommendation
-    recommendation
-    recommendation
+    recommendation generateRecommendation(Long intentId);
+    ->fetch intent
+    ->fetch active cards for user
+    ->fetch active rules for card + category
+    ->calculated reward
+    
+    recommendation getRecommendationById(Long id);
+    recommendation getRecommendationByUSer(Long userId);
+    recommendation getAllRecommendations();
 }
