2aefc935c7d0c2eb1f431d2ac317520343920306
bala-amypo <bala@amypo.in>
2025-12-21 17:16:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/rewardrulerepository.java
+++ src/main/java/com/example/demo/repository/rewardrulerepository.java
@@ -1,12 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.userprofile;
+import com.example.demo.entity.rewardrule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RewardRuleRepository extends JpaRepository<RewardRule, Long> {
-    @Query("SELECT r FROM RewardRule r WHERE r.active = true")
-    List<RewardRule> findByActiveTrue();
+public interface rewardrulerepository extends JpaRepository<rewardrule, Long> {
+    @Query("SELECT r FROM rewardrule r WHERE r.active = true")
+    List<rewardrule> findByActiveTrue();
 }
--- src/main/java/com/example/demo/service/rewardruleservice.java
+++ src/main/java/com/example/demo/service/rewardruleservice.java
@@ -1,4 +1,6 @@
 package com.example.demo.service;
 public class rewardruleservice{
-    
+    rewardrule createRule(rewardrule rule);
+    rewardrule updateRule(Long id,rewardrule updated);
+    rewardrule getRulesByCard(Long )
 }
