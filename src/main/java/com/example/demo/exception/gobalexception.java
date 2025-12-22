b25df646d78178d3ef79352df6c4925c5beffad2
bala-amypo <bala@amypo.in>
2025-12-22 10:00:22 +0000

    User commit

--- src/main/java/com/example/demo/exception/gobalexception.java
+++ src/main/java/com/example/demo/exception/gobalexception.java
@@ -1,5 +1,6 @@
 package com.example.demo.exception;
-
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,13 +9,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class gobalexception {
 
-    @ExceptionHandler(resourcenotexception.class)
-    public ResponseEntity<String> handleNotFound(resourcenotexception ex) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(badrequestexception.class)
-    public ResponseEntity<String> handleBadRequest(badrequestexception ex) {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
