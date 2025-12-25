aceafd9c500fe04023416383619b3dca4f58e09b
bala-amypo <bala@amypo.in>
2025-12-25 10:41:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserProfile.java
+++ src/main/java/com/example/demo/entity/UserProfile.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Data;
 
+@Data
 @Entity
 public class UserProfile {
 
@@ -17,5 +19,45 @@ public class UserProfile {
     private String password;
     private String role;
 
-    // getters & setters
+    // ===== GETTERS =====
+    public Long getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    // ===== SETTERS =====
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
