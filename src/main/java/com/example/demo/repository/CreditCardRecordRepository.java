ea969b1b50ec336363979da228d8175cb809fb75
bala-amypo <bala@amypo.in>
2025-12-25 10:26:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/CreditCardRecordRepository.java
+++ src/main/java/com/example/demo/repository/CreditCardRecordRepository.java
@@ -1,11 +1,7 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.CreditCardRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CreditCardRecordRepository extends JpaRepository<CreditCardRecord, Long> {
+public interface CreditCardRecordRepository
+        extends JpaRepository<CreditCardRecord, Long> {
 
     List<CreditCardRecord> findByUserId(Long userId);
+
+    List<CreditCardRecord> findActiveCardsByUserId(Long userId);
 }
