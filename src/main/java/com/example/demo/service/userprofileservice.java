c60875892976029306792cee6cc97fa8fce9b6b6
bala-amypo <bala@amypo.in>
2025-12-25 09:53:40 +0000

    User commit

--- src/main/java/com/example/demo/service/userprofileservice.java
+++ src/main/java/com/example/demo/service/userprofileservice.java
@@ -1,14 +1,9 @@
 package com.example.demo.service;
 import com.example.demo.entity.userprofile;
 import java.util.List;
-import java.util.Optional;
 
-public interface userprofileservice {
-
-    userprofile createUser(userprofile profile);
-    userprofile getUserById(Long id);
-    userprofile findByUserId(String userId);
-    List<userprofile> getAllUsers();
-    Optional<userprofile> findByEmail(String email);
-    userprofile updateUserStatus(Long id, boolean active);
+public interface UserProfileService {
+    UserProfile createUser(UserProfile u);
+    UserProfile getUserById(Long id);
+    List<UserProfile> getAllUsers();
 }
