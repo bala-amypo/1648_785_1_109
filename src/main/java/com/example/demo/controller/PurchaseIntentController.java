4d39510de45077ef130526a3140f029a7eae8e88
bala-amypo <bala@amypo.in>
2025-12-26 09:12:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/PurchaseIntentController.java
+++ src/main/java/com/example/demo/controller/PurchaseIntentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/HBKD VFHJ ")
+@RestController
 @RequestMapping("/api/intents")
 public class PurchaseIntentController {
 
