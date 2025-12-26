22e86338a44818edc8b7041b054124a91cb2d958
bala-amypo <bala@amypo.in>
2025-12-26 05:53:53 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseIntentService.java
+++ src/main/java/com/example/demo/service/PurchaseIntentService.java
@@ -1,12 +1,12 @@
+
 package com.example.demo.service;
 
-import com.example.demo.entity.PurchaseIntent;
-
+import com.example.demo.entity.PurchaseIntentRecord;
 import java.util.List;
 
 public interface PurchaseIntentService {
-    PurchaseIntent addIntent(PurchaseIntent intent);
-    PurchaseIntent getIntentById(Long id);
-    List<PurchaseIntent> getIntentsByUser(Long userId);
-    List<PurchaseIntent> getAllIntents();
+    PurchaseIntentRecord createIntent(PurchaseIntentRecord intent);
+    PurchaseIntentRecord getIntentById(Long id);
+    List<PurchaseIntentRecord> getAllIntents();
+    List<PurchaseIntentRecord> getIntentsByUser(Long userId);
 }
