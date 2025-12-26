c1a7745edd022cd543478a040a910ab891e02315
bala-amypo <bala@amypo.in>
2025-12-26 04:47:45 +0000

    User commit

--- src/main/java/com/example/demo/entity/RecommendationRecord.java
+++ src/main/java/com/example/demo/entity/RecommendationRecord.java
@@ -1,3 +1,7 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
 @Entity
 public class RecommendationRecord {
 
@@ -11,26 +15,11 @@ public class RecommendationRecord {
     private double expectedRewardValue;
     private String calculationDetailsJson;
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public void setPurchaseIntentId(Long purchaseIntentId) {
-        this.purchaseIntentId = purchaseIntentId;
-    }
-
-    public void setRecommendedCardId(Long recommendedCardId) {
-        this.recommendedCardId = recommendedCardId;
-    }
-
-    public void setExpectedRewardValue(double expectedRewardValue) {
-        this.expectedRewardValue = expectedRewardValue;
-    }
-
+    public void setUserId(Long userId) { this.userId = userId; }
+    public void setPurchaseIntentId(Long purchaseIntentId) { this.purchaseIntentId = purchaseIntentId; }
+    public void setRecommendedCardId(Long recommendedCardId) { this.recommendedCardId = recommendedCardId; }
+    public void setExpectedRewardValue(double expectedRewardValue) { this.expectedRewardValue = expectedRewardValue; }
     public void setCalculationDetailsJson(String calculationDetailsJson) {
         this.calculationDetailsJson = calculationDetailsJson;
     }
 }
-find src/main/java -name "*CreditCardRecord*.java"
-find src/main/java -name "*RewardRule*.java"
-find src/main/java -name "*RecommendationRecord*.java"
