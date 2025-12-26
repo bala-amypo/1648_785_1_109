91408419700d5c02c7061a09922a77010bacb432
bala-amypo <bala@amypo.in>
2025-12-26 04:11:47 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -0,0 +1,61 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.*;
+import com.example.demo.entity.UserProfile;
+import com.example.demo.repository.UserProfileRepository;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.AuthService;
+import com.example.demo.service.UserProfileService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class AuthServiceImpl implements AuthService {
+
+    private final UserProfileService userService;
+    private final UserProfileRepository userRepo;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    public AuthServiceImpl(UserProfileService userService,
+                           UserProfileRepository userRepo,
+                           AuthenticationManager authenticationManager,
+                           JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.userRepo = userRepo;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public JwtResponse login(LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(), request.getPassword()));
+
+        UserProfile user = userRepo.findByEmail(request.getEmail()).orElseThrow();
+
+        String token = jwtUtil.generateToken(
+                user.getId(), user.getEmail(), user.getRole());
+
+        return new JwtResponse(token, user.getId(), user.getEmail(), user.getRole());
+    }
+
+    @Override
+    public JwtResponse register(RegisterRequest request) {
+
+        UserProfile user = new UserProfile();
+        user.setUserId(request.getUserId());
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRole(request.getRole());
+
+        UserProfile saved = userService.createUser(user);
+
+        String token = jwtUtil.generateToken(
+                saved.getId(), saved.getEmail(), saved.getRole());
+
+        return new JwtResponse(token, saved.getId(), saved.getEmail(), saved.getRole());
+    }
+}
