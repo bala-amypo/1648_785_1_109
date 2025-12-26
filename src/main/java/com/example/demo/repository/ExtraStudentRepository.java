3f5560bc90339129ecfa4cbf5ae8637e88e26dcd
bala-amypo <bala@amypo.in>
2025-12-26 16:11:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExtraStudentRepository.java
+++ src/main/java/com/example/demo/repository/ExtraStudentRepository.java
@@ -2,10 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ExtraStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-@Repository
-public interface ExtraStudentRepo extends JpaRepository<ExtraStudent, Long> {
+public interface ExtraStudentRepository extends JpaRepository<ExtraStudent, Long> {
     Optional<ExtraStudent> findByEmail(String email);
 }
--- src/main/java/com/example/demo/service/ExtraStudentService.java
+++ src/main/java/com/example/demo/service/ExtraStudentService.java
@@ -1,23 +1,8 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ExtraStudent;
-import com.example.demo.repository.ExtraStudentRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service // This fixes the 'UnsatisfiedDependencyException'
-public class ExtraStudentServiceImpl implements ExtraStudentService {
-
-    @Autowired
-    private ExtraStudentRepo repo;
-
-    @Override
-    public ExtraStudent saveExtraStudent(ExtraStudent student) {
-        return repo.save(student);
-    }
-
-    @Override
-    public ExtraStudent CheckEmail(String email) {
-        return repo.findByEmail(email).orElse(null);
-    }
+public interface ExtraStudentService {
+    ExtraStudent saveExtraStudent(ExtraStudent student);
+    ExtraStudent CheckEmail(String email);
 }
