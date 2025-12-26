44fdc697563206ce9d845eed4a75f576ce157ddb
bala-amypo <bala@amypo.in>
2025-12-26 05:32:44 +0000

    User commit

--- src/main/java/com/example/demo/entity/CreditCardRecord.java
+++ src/main/java/com/example/demo/entity/CreditCardRecord.java
@@ -9,7 +9,103 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data
+@Datapackage com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CreditCardRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String cardNumber;
+    private String cardHolderName;
+    private String cardType;
+    private Double balance;
+
+    // No-args constructor
+    public CreditCardRecord() {
+    }
+
+    // All-args constructor
+    public CreditCardRecord(Long id, Long userId, String cardNumber, String cardHolderName, String cardType, Double balance) {
+        this.id = id;
+        this.userId = userId;
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.cardType = cardType;
+        this.balance = balance;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "CreditCardRecord{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", cardHolderName='" + cardHolderName + '\'' +
+                ", cardType='" + cardType + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
+
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreditCardRecord {
--- src/main/java/com/example/demo/entity/RewardRule.java
+++ src/main/java/com/example/demo/entity/RewardRule.java
@@ -4,14 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class RewardRule {
 
     @Id
@@ -19,8 +13,62 @@ public class RewardRule {
     private Long id;
 
     private String ruleName;
-
     private String description;
-
     private Double rewardPercentage;
+
+    // No-args constructor
+    public RewardRule() {
+    }
+
+    // All-args constructor
+    public RewardRule(Long id, String ruleName, String description, Double rewardPercentage) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.description = description;
+        this.rewardPercentage = rewardPercentage;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getRewardPercentage() {
+        return rewardPercentage;
+    }
+
+    public void setRewardPercentage(Double rewardPercentage) {
+        this.rewardPercentage = rewardPercentage;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "RewardRule{" +
+                "id=" + id +
+                ", ruleName='" + ruleName + '\'' +
+                ", description='" + description + '\'' +
+                ", rewardPercentage=" + rewardPercentage +
+                '}';
+    }
 }
