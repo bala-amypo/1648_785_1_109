2b94b3a6788661f67e225ba2271a0e32741fb095
bala-amypo <bala@amypo.in>
2025-12-27 05:37:17 +0000

    User commit

--- src/main/java/com/example/demo/service/CreditCardService.java
+++ src/main/java/com/example/demo/service/CreditCardService.java
@@ -1,12 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.CreditCardRecord;
-
 import java.util.List;
 
 public interface CreditCardService {
     CreditCardRecord addCard(CreditCardRecord card);
-    CreditCardRecord getCardById(Long id);
     List<CreditCardRecord> getCardsByUser(Long userId);
     List<CreditCardRecord> getAllCards();
 }
