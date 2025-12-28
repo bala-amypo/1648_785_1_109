55acb42db7b0177a8bab855f2aa7b957be737bc1
bala-amypo <bala@amypo.in>
2025-12-28 19:36:23 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,35 +1,20 @@
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        final String securitySchemeName = "bearerAuth";
-
         return new OpenAPI()
-                .info(new Info()
-                        .title("User Profile API")
-                        .version("1.0")
-                        .description("API for managing user profiles. Use /auth/login to get JWT token."))
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
-    }
+
+                .servers(List.of(
+                        new Server().url("https://9173.pro604cr.amypo.ai/")
+                ));
+        }
 }
