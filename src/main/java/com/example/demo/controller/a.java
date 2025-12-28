1f4c96d3b5d32268893f4b33449fa07718699b04
bala-amypo <bala@amypo.in>
2025-12-28 19:08:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/a.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Credit Card Reward Maximizer API is running successfully";
+    }
+}
